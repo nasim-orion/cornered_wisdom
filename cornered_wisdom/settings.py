4ac45afa35d97f4a5dac124d42cda617ea21dd44
nasim-orion <nasimch2709@gmail.com>
2026-09-14 02:39:54 +0100

    initial deploy

--- cornered_wisdom/settings.py
+++ cornered_wisdom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s5s9d8u$05e!eyp#@h8x#o547(j)1s4qup19ix2h!$pgzxrqzy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.herokuapp.com']
 
 
 # Application definition
